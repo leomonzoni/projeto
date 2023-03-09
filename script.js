fdc25f8b90562b76c61f2c5a0f0ad4335c1ec673
Leonardo Monzoni <leonardo_monzoni@hotmail.com>
2023-03-08 21:35:06 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a a tag img
+
+  const img = document.querySelector("#profile img")
+  // susbstituir a imagem
+
+  if (html.classList.contains("light")) {
+    //se tiver light mode, add a img light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se tiver sem light mode, add a img normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
